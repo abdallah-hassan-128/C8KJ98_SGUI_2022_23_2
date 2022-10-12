dc78e803ec364ca4db8cff343538d34b48eecaf9
abdallah-hassan-128 <abdallah.imad128@gmail.com>
2022-10-12 23:28:51 +0200

    the empty projects

---
+++ C8KJ98_ADT_2022_23_1/ABC123_ADT_2022_23_1.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC123_ADT_2022_23_1.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
