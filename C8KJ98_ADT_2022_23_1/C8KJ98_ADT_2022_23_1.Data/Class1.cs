1819f56ac28afe4654cf3fa0ada4adf686399f5c
abdallah-hassan-128 <abdallah.imad128@gmail.com>
2022-10-13 00:04:36 +0200

    nuget packages and idf , mdf files

--- C8KJ98_ADT_2022_23_1/C8KJ98_ADT_2022_23_1.Data/Class1.cs
+++ C8KJ98_ADT_2022_23_1/C8KJ98_ADT_2022_23_1.Data/Class1.cs
@@ -4,5 +4,7 @@ namespace C8KJ98_ADT_2022_23_1.Data
 {
     public class Class1
     {
+        // Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename="|DataDirectory|\Database1.mdf";Integrated Security = True; MultipleActiveResultSets=True
+
     }
 }
