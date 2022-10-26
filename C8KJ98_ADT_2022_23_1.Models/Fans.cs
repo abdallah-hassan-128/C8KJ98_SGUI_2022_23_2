898ac825a055f5efc380748a519b5ac1de0e5827
abdallah-hassan-128 <abdallah.imad128@gmail.com>
2022-10-26 21:59:19 +0200

    created and filled Model Layer classes and DbContextClass

---
+++ C8KJ98_ADT_2022_23_1.Models/Fans.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C8KJ98_ADT_2022_23_1.Models
+{
+    class Fans
+    {
+    }
+}
