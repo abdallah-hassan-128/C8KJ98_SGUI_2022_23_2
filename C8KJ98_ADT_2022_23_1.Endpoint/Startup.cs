9cb2cf7019e9f67a1cd85a2e2ba07b2208f5ea04
abdallah-hassan-128 <abdallah.imad128@gmail.com>
2022-12-04 22:11:43 +0100

    trying to fix endpoints

--- C8KJ98_ADT_2022_23_1.Endpoint/Startup.cs
+++ C8KJ98_ADT_2022_23_1.Endpoint/Startup.cs
@@ -7,20 +7,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using C8KJ98_ADT_2022_23_1.Data;
 using C8KJ98_ADT_2022_23_1.Logic;
 using C8KJ98_ADT_2022_23_1.Repository;
+
 
 
 namespace C8KJ98_ADT_2022_23_1.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+                                                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
             services.AddTransient<IFansLogic, FansLogic>();
             services.AddTransient<IArtistsLogic, ArtistsLogic>();
@@ -32,10 +36,9 @@ namespace C8KJ98_ADT_2022_23_1.Endpoint
             services.AddTransient<IReservationsRepository, ReservationsRepository>();
             services.AddTransient<IReservationsServicesRepository, ReservationsServicesRepository>();
             services.AddTransient<IServicesRepository, ServicesRepository>();
-            services.AddTransient<TalkWithYourFavoriteArtistDbContext, TalkWithYourFavoriteArtistDbContext>();
+            services.AddTransient<DbContext, TalkWithYourFavoriteArtistDbContext>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
