e75832efa806cfbf75bcb72b657916d879eee346
abdallah-hassan-128 <abdallah.imad128@gmail.com>
2023-05-30 13:49:59 +0200

    SignalR and JSclient.

---
+++ C8KJ98_ADT_2022_23_1.Endpoint/Services/SignalRHub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C8KJ98_ADT_2022_23_1.Endpoint.Services
+{
+    public class SignalRHub
+    {
+    }
+}
